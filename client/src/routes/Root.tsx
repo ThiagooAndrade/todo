9c28c4dbfeeffba4d253a99c77bdb07c3876f42a
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-08-30 17:41:37 -0300

    feat: finished input and button create

--- client/src/routes/Root.tsx
+++ client/src/routes/Root.tsx
@@ -3,17 +3,17 @@ import HeaderRoot from "../components/HeaderRoot";
 
 export default function Root() {
     return (
-        <>
+        <div className="overflow-hidden h-screen">
             <HeaderRoot />
-            <div className="w-screen bg-darkContent h-32">
-                <div className="container">
-                    <div className="mt-[-10px] flex gap-2 py-2 px-4">
-                        <input placeholder="Adicione uma tarefa" className="bg-darkInput px-2 border-black rounded-md w-10 flex-grow" type="text" name="" id="" />
-                        <button className="text-white bg-blue-300 rounded-md p-2 text-sm">Criar +</button>
+            <div className="w-screen h-full bg-darkContent">
+                <div className="container relative h-20">
+                    <div className="flex gap-2 py-2 px-4 md:px-8 absolute container bottom-12">
+                        <input placeholder="Adicione uma nova tarefa" className=" bg-darkInput px-2 md:py-2 lg:py-3 border-[1px] border-black rounded-md w-10 flex-grow placeholder:text-darkInputPlaceholder font-normal caret-white" type="text" name="" id="" />
+                        <button className="text-white bg-blue-600 rounded-md p-2 text-sm">Criar +</button>
                     </div>
                 </div>
             </div>
 
-        </>
+        </div>
     )
 }
