0f87af016685a920b78dd29dce708cd8e8db50ff
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-08-30 16:39:29 -0300

    add: header component

---
+++ client/src/components/HeaderRoot.tsx
@@ -0,0 +1,11 @@
+import rocketIcon from "/rocket.png"
+export default function HeaderRoot() {
+    return (
+        <header className="w-screen bg-gray-950 h-32 font-sans">
+            <div className="container h-full text-white flex justify-center items-center gap-2">
+                <img src={rocketIcon} alt="Todo Icon" />
+                <h1 className="text-blue-400 text-2xl font-bold">to<span className="text-purple-500">do</span></h1>
+            </div>
+        </header>
+    )
+}
