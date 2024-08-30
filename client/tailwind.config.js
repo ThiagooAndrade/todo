6b75a5c5a3e8a4622b4845e1d80e6bb8812f5a93
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-08-30 16:35:39 -0300

    add: font-family Poppins and container for responsivity

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "sans": ["Poppins", "sans-serif"],
+      },
+      container: {
+        center: true,
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+        }
+      }
+    },
   },
   plugins: [],
 }
