9b5a2f754a2a1cceba111eb627aa7c8f1bf01ae7
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-08-31 12:00:51 -0300

    style: size of h1

--- client/src/components/HeaderRoot.tsx
+++ client/src/components/HeaderRoot.tsx
@@ -4,7 +4,7 @@ export default function HeaderRoot() {
         <header className="w-screen bg-darkHeader h-48 font-sans">
             <div className="container h-full text-white flex justify-center items-center gap-2">
                 <img src={rocketIcon} alt="Todo Icon" />
-                <h1 className="text-blue-400 text-2xl font-bold">to<span className="text-purple-500">do</span></h1>
+                <h1 className="text-blue-400 text-2xl lg:text-4xl font-bold">to<span className="text-purple-500">do</span></h1>
             </div>
         </header>
     )
