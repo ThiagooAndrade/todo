f04ffab8bd09d3ab1c0fd749a03a20139afa70ad
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-09-01 16:24:04 -0300

    fix: checkbox shadcnui component

--- client/src/components/Tasks.tsx
+++ client/src/components/Tasks.tsx
@@ -2,7 +2,7 @@
 
 export function Tasks() {
     return (
-        <div className="bg-gray-300 dark:bg-zinc-800 rounded-md p-2">
+        <div className="bg-gray-300 dark:bg-zinc-800 rounded-md  p-2">
             <h1>Hello</h1>
         </div>
     )
