d8d9e45725bb2a9feca1c640d4f27f483c5e13c7
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-09-01 15:49:11 -0300

    add: shadcnui components

--- client/src/components/HeaderRoot.tsx
+++ client/src/components/HeaderRoot.tsx
@@ -10,14 +10,14 @@ export default function HeaderRoot() {
         <header className="w-screen h-48 font-sans relative z-0 bg-lightHeader dark:bg-darkHeader duration-500">
             <div className="container h-full text-white dark:text-black flex justify-center items-center space-x-2">
                 <img src={rocketIcon} alt="Todo Icon" />
-                <h1 className="text-blue-400 text-2xl lg:text-4xl font-bold">to<span className="text-purple-500">do</span></h1>
+                <h1 className="text-blue-100 dark:text-blue-400 text-2xl lg:text-4xl font-bold">to<span className="text-purple-500">do</span></h1>
             </div>
             <div className="absolute right-2 top-2 text-white flex items-center gap-2 duration-500">
                 {theme == "light" && (
-                    <Sun size={20} strokeWidth={2} className="text-black" />
+                    <Sun size={20} strokeWidth={2} />
                 )}
                 {theme == "dark" && (
-                    <Moon size={20} strokeWidth={2} className="text-white" />
+                    <Moon size={20} strokeWidth={2} />
                 )}
                 <Switch id="themeMode" className="" defaultChecked={true} onCheckedChange={toggleTheme} />
             </div>
