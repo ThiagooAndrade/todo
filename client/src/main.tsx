d1cc15c4445e76609426723434811381a6c4c6b4
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-09-07 22:43:14 -0300

    feat: add DatesProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { DatesContextProvider } from './hooks/useDates'
 import router from './routes'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <DatesContextProvider>
+      <RouterProvider router={router} />
+    </DatesContextProvider>
   </StrictMode>,
 )
